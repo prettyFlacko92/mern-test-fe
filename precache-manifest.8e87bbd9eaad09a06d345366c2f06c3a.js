23bca7733ab7c867288274d9ea71796aaa781763
IgorNovikov10 <igor.novikov@ukad-group.com>
2024-09-16 23:55:18 +0300

    Updates

---
+++ precache-manifest.8e87bbd9eaad09a06d345366c2f06c3a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "282ee3ca3797548541e6da94dcc8c15d",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "de41eff00071517f62ae",
+    "url": "/react-gh-pages/static/css/3.5c9d975e.chunk.css"
+  },
+  {
+    "revision": "81b1f01d797ce3277f94",
+    "url": "/react-gh-pages/static/css/4.0a848c59.chunk.css"
+  },
+  {
+    "revision": "61ed2d7d1e54fa7de647",
+    "url": "/react-gh-pages/static/css/5.5e8c5c49.chunk.css"
+  },
+  {
+    "revision": "788882ccc4f3501b2ff5",
+    "url": "/react-gh-pages/static/css/6.efd60c72.chunk.css"
+  },
+  {
+    "revision": "10f82926f89e1fcb19a0",
+    "url": "/react-gh-pages/static/css/7.cfadcd7f.chunk.css"
+  },
+  {
+    "revision": "8509c3150349e4b42073",
+    "url": "/react-gh-pages/static/css/main.75f6faf7.chunk.css"
+  },
+  {
+    "revision": "dcea82b213e521620e7c",
+    "url": "/react-gh-pages/static/js/2.92aeb64f.chunk.js"
+  },
+  {
+    "revision": "de41eff00071517f62ae",
+    "url": "/react-gh-pages/static/js/3.ec08375a.chunk.js"
+  },
+  {
+    "revision": "81b1f01d797ce3277f94",
+    "url": "/react-gh-pages/static/js/4.03b22c64.chunk.js"
+  },
+  {
+    "revision": "61ed2d7d1e54fa7de647",
+    "url": "/react-gh-pages/static/js/5.47a2cf0a.chunk.js"
+  },
+  {
+    "revision": "788882ccc4f3501b2ff5",
+    "url": "/react-gh-pages/static/js/6.b6b942db.chunk.js"
+  },
+  {
+    "revision": "10f82926f89e1fcb19a0",
+    "url": "/react-gh-pages/static/js/7.ff1a9b4a.chunk.js"
+  },
+  {
+    "revision": "8509c3150349e4b42073",
+    "url": "/react-gh-pages/static/js/main.2aebb101.chunk.js"
+  },
+  {
+    "revision": "8ba254f2eef115bbf08d",
+    "url": "/react-gh-pages/static/js/runtime-main.79b14c0d.js"
+  }
+]);
